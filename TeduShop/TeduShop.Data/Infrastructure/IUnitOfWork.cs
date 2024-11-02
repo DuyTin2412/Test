43a471718959df81d9c8e764d449f6b61a0cdd47
DuyTin2412 <102210080@sv1.dut.udn.vn>
2024-11-02 11:22:23 +0700

    change v3

---
+++ TeduShop/TeduShop.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public interface IUnitOfWork // dinh nghia cho cac giao tiep giua cac class
+    {
+        void Commit();
+    }
+}
